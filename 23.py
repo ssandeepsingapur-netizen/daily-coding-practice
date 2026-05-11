07d460f0233f399637848eb5a62e5563a539ac29
sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>
2026-05-11 09:47:44 -0700

    Add files via upload
    Signed-off-by: sandeep singapur <274041123+ssandeepsingapur-netizen@users.noreply.github.com>

--- 23.py
+++ 23.py
@@ -1,12 +1,11 @@
-class Animal:
-    def speak(self):
-        print("animal makes sound")
+class book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
-class dog(Animal):
-    def bark(self):
-        print("Dog barks")
-
-d = dog()
-d.speak()
-d.bark()
-
+    def display(self):
+        print(f"Title: {self.title}, Author: {self.author}")
+title = input("Enter the book title: ")
+author = input("Enter the book author: ")
+my_book = book(title, author)
+my_book.display()
